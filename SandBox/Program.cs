259e6a861596fec4af0a74568be59046b40ce28f
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-28 14:23:36 +0200

    slice file into 4 pieces

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -9,17 +9,48 @@ namespace SandBox
         {
             //var streamReader = new StreamReader(path:@"text.txt");
 
-            using (var streamReader = new StreamReader(path: @"text.txt"))
+            //using (var streamReader = new StreamReader(path: @"text.txt"))
+            //{
+            //    while (!streamReader.EndOfStream)
+            //    {
+            //        string line = streamReader.ReadLine();
+            //    }
+            //}
+
+            // Console.WriteLine(Environment.CurrentDirectory);
+
+            //File.WriteAllText("output.txt", sb.ToString());
+
+            //slice file into 4 pieces
+            var fileLenght = new FileInfo("text.txt").Length;
+            var partSize = fileLenght / 4;
+            int parts = 4;
+            int fileNum = 1;
+
+            using (var streamRaeder = new StreamReader("text.txt"))
             {
-                while (!streamReader.EndOfStream)
+                while (!streamRaeder.EndOfStream)
                 {
-                    string line = streamReader.ReadLine();
+                    var bufferSize = partSize;
+
+                    if (fileNum == parts)
+                    {
+                        bufferSize = fileLenght - (parts - 1) * partSize;
+                    }
+
+                    var buffer = new char[bufferSize];
+                    streamRaeder.Read(buffer, 0, buffer.Length);
+
+                    using (var writer = new StreamWriter($"result{fileNum}.txt"))
+                    {
+                        writer.Write(buffer);
+                    }
+                    fileNum++;
                 }
             }
 
-            Console.WriteLine(Environment.CurrentDirectory);
 
-            //File.WriteAllText("output.txt", sb.ToString());
+            
         }
     }
 }
