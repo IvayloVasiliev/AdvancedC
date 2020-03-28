93f383f699ee4d9c765e293bc314ec5041fd33b6
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-28 13:18:31 +0200

    Add project files.

---
+++ EvenLines/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader(path:@"../../../text.txt"))
+            using (var streamWriter = new StreamWriter("write.txt"))
+            {
+                int lineCounter = 0;
+                var symbolToReplace = new[] { "-", ",", ".", "!", "?" };
+
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+
+                    if (lineCounter % 2 == 0)
+                    {
+                        var words = line.Split();
+
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            var curentWord = words[i];
+
+                            foreach (var symbol in symbolToReplace)
+                            {
+                                curentWord = curentWord.Replace(symbol, "@");
+                            }
+                            words[i] = curentWord;
+                        }
+                         
+                        var result = string.Join(" ", words.Reverse());
+                        streamWriter.WriteLine(result);
+                    }
+                    lineCounter++; 
+                }
+            }
+        }
+    }
+}
---
+++ SandBox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var streamReader = new StreamReader(path:@"text.txt");
+
+            using (var streamReader = new StreamReader(path: @"text.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                }
+            }
+
+            Console.WriteLine(Environment.CurrentDirectory);
+
+            //File.WriteAllText("output.txt", sb.ToString());
+        }
+    }
+}
