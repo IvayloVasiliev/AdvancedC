502e91f593c20466a184cd7b7ec2fddaced7d9ce
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-28 21:58:04 +0200

    Predicate

---
+++ CustomMinFunct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int[], int> minFunc = inputNums =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var currrentNum in inputNums)
+                {
+                    if (currrentNum < minValue)
+                    {
+                        minValue = currrentNum;
+                    }
+                }
+
+                return minValue;
+            };
+
+
+            int[] numbres = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+           int result = minFunc(numbres);
+
+            Console.WriteLine(result    );
+        }
+    }
+}
---
+++ FindEvenOrOdds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvenOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEvenNum = num => num % 2 == 0;
+             
+            Action<List<int>> print = nums => Console.WriteLine(string.Join(" ", nums));
+
+            int[] input = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            List<int> numbers = new List<int>();
+
+            int startNum = input[0];
+            int endNum = input[1];
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string numType = Console.ReadLine();
+
+            if (numType == "even")
+            {
+                numbers.RemoveAll(x => !isEvenNum(x));
+            }
+            else
+            {
+                numbers.RemoveAll(x => isEvenNum(x));
+            }
+
+            print(numbers);
+        }
+    }
+}
