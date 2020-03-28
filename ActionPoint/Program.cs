6823a407d9e057aa698d421ad2ac61b4ebf79a09
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-28 21:06:20 +0200

    Action

---
+++ ActionPoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action<string> print = Console.WriteLine;
+
+            Action<string> print = item => Console.WriteLine(item);
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(print);
+
+            //Action<string[]> print = items => Console.WriteLine(string.Join(Environment.NewLine, items));
+
+            //string[] names = Console.ReadLine().Split().ToArray();
+
+            //print(names);
+                       
+        }
+    }
+}
